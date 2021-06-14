6253582ad728c0ba5c524387f67e64ac19127cdd
vojta <vojta@ne.cz>
2021-06-14 17:29:43 +0200

    Initial Commit

---
+++ MonumentaApp/MonumentaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonumentaAppApp.swift
+//  MonumentaApp
+//
+//  Created by vojta on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MonumentaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
